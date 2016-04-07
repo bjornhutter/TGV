1f8f9789f0160f2c2fc36af3b0c7b08239622ac9
Bjorn Hutter <bjornhutter@gmail.com>
2016-04-07 14:35:00 +0200

    Added template and simple styling for send_script.php page

--- send_script.php
+++ send_script.php
@@ -15,8 +15,45 @@
 <header>
 
 </header>
-
 <?php include('includes/navigation.inc') ?>
+
+<main>
+    <!--TABBAR-->
+</main>
+<section class="script-form-wrapper">
+    <div class="script-form-inner-wrapper">
+        <form action="" method="post" class="script-form">
+            <ul class="script-form-ul">
+                <li class="script-form-li">
+                    <p>Förnamn: </p>
+                    <input type="text" name="fname" placeholder="Förnamn" class="script-form-input" required>
+                </li>
+                <li class="script-form-li">
+                    <p>Efternamn: </p>
+                    <input type="text" name="lname" placeholder="Efternamn" class="script-form-input" required>
+                </li>
+                <li class="script-form-li">
+                    <p>Din emailaddress: </p>
+                    <input type="email" name="email" title="Email" placeholder="Din Email" class="script-form-input"
+                           required>
+                </li>
+                <li class="script-form-li">
+                    <p>Ämne: </p>
+                    <input type="text" name="topic" title="Ämne" placeholder="Ämne" class="script-form-input" required>
+                </li>
+                <li class="script-form-li">
+                    <p>Meddelande: </p>
+                    <textarea name="message" title="Meddelande" rows="10" cols="50"
+                              class="script-form-input"></textarea>
+                </li>
+                <li class="script-form-li">
+                    <input type="submit" name="submit" value="Skicka Manus" class="script-form-input-submit">
+                </li>
+                <!--LÄGG TILL BIFOGA FIL FÖR MANUS -->
+            </ul>
+        </form>
+    </div>
+</section>
 
 <?php include('includes/footer.inc') ?>
 </body>
