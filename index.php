ace777f28cc5c71faaf8c2804e792489a93d7700
Bjorn Hutter <bjornhutter@gmail.com>
2016-04-07 09:35:08 +0200

    Added index.php template

--- index.php
+++ index.php
@@ -2,11 +2,22 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="css/css-reset.css">
     <link rel="stylesheet" type="text/css" href="css/master.css">
     <title>Tidskrift för genusvetenskap | TGV</title>
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 </head>
 <body>
-<p>Hej hej !</p>
+
+<!--Header Start-->
+
+<!--Header End-->
+
+<?php include('includes/navigation.inc') ?>
+
+<?php include('includes/footer.inc') ?>
 </body>
 </html>
