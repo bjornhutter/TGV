fc5840457fd8eea5f4a7e3d8aeffaa9c784117d1
Julia Nilstal <nilstal@hotmail.com>
2016-04-13 15:03:42 +0200

    added editbutton, form to edit, editprocess

--- contact.php
+++ contact.php
@@ -16,29 +16,39 @@
 
 </header>
 <?php include('includes/navigation.inc') ?>
+<?php
+include('includes/db_connect.inc');
+$result = mysqli_query($link, "SELECT * FROM contact") or die(mysqli_error());
 
-<main class="contact-wrapper">
-    <section class="contact-info">
-        <ul class="contact-info-ul">
-            <li class="contact-info-li">
-                <p>List objekt 1</p>
-            </li>
-            <li class="contact-info-li">
-                <p>List objekt 2</p>
-            </li>
-            <li class="contact-info-li">
-                <p>List objekt 3</p>
-            </li>
-            <li class="contact-info-li">
-                <p>List objekt 4</p>
-            </li>
-        </ul>
-    </section>
-    <section class="contact-map">
-        <div class="map">
+echo '<main class="contact-wrapper">';
+echo '<section class="contact-info">';
+echo '<ul class="contact-info-ul">';
+while ($row = mysqli_fetch_array($result)) {
 
-        </div>
-    </section>
+    $id = $row['id'];
+    $title = $row['title'];
+    $adress = $row['adress'];
+    $phone = $row['phone'];
+    $email = $row['email'];
+
+    echo '<p>' . $title . '</p>';
+    echo '<li class="contact-info-li">' . $adress . '</li>';
+    echo '<li class="contact-info-li">' . $phone . '</li>';
+    echo '<li class="contact-info-li">' . $email . '</li>';
+
+    if (isset($_SESSION['user'])) {
+        echo '<p><a href="contact_edit.php?id=' . $id . '">Redigera</a></p>';
+    }
+    echo '</ul>';
+    echo '</section>';
+}
+?>
+</section>
+<section class="contact-map">
+    <div class="map">
+
+    </div>
+</section>
 </main>
 
 <?php include('includes/footer.inc') ?>
--- contact_edit_process.php
+++ contact_edit_process.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Admin
- * Date: 2016-04-13
- * Time: 14:56
- */
+include('includes/db_connect.inc');
+//require('includes/auth.php');
+$id = $_POST['id'];
+
+if (isset($_POST['save'])) {
+    $title = $_POST['title'];
+    $adress = $_POST['adress'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+
+    mysqli_query($link, "UPDATE contact SET title = '$title', adress = '$adress', phone = 'phone', email = '$email' WHERE id = '$id'");
+}
+
+header("Location: contact.php");
+
+?>
