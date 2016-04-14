a19ec4b7e5d7ed987056857dea4b8d531a2b38b0
Julia Nilstal <nilstal@hotmail.com>
2016-04-14 15:54:42 +0200

    added headinfo in editfile

--- about_editors_edit.php
+++ about_editors_edit.php
@@ -20,7 +20,14 @@ $imgName = $row['image'];
 <!doctype html>
 <html>
 <head>
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/css-reset.css">
+    <link rel="stylesheet" type="text/css" href="css/master.css">
+    <title>Redigera redaktörer | Tidskrift för genusvetenskap</title>
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
 </head>
 
 <body>
