9d77b6cbd0cf7e319190b88c46535479ddf3b5fc
Julia Nilstal <nilstal@hotmail.com>
2016-04-14 16:09:46 +0200

    added form to submit editors and upload img and added process

--- about_editors_process.php
+++ about_editors_process.php
@@ -1,8 +1,12 @@
 <?php
-$target_dir = "uploads/";
+include('includes/db_connect.inc');
+//require('includes/auth.php');
+
+$target_dir = "uploads";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
@@ -17,33 +21,43 @@ if(isset($_POST["submit"])) {
 
 // Check if file already exists
 if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
+    echo "Filen existerar redan.";
     $uploadOk = 0;
 }
 
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
+    echo "Filen är för stor";
     $uploadOk = 0;
 }
 
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+    echo "Endast JPG, JPEG & PNG är tillåtet.";
     $uploadOk = 0;
 }
 
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
+    echo "Din fil blev ej uppladdad.";
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        chmod($target_file, 0755);
     } else {
-        echo "Sorry, there was an error uploading your file.";
+        echo "Det blev ett fel när du försökte ladda upp din fil.";
     }
 }
+
+$imgName = basename($_FILES["fileToUpload"]["name"]);
+
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$content = $_POST['content'];
+
+mysqli_query($link, "INSERT INTO tgv_about_editors (fname, lname, content, image) VALUES ('$fname', '$lname', '$content', '$imgName')") or die(mysqli_error());
+
+header('Location: about.php')
 
 ?>
---
+++ editors_form.php
@@ -0,0 +1,36 @@
+<?php
+//require('includes/auth.php');
+?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/css-reset.css">
+    <link rel="stylesheet" type="text/css" href="css/master.css">
+    <title>Lägg till redaktör| Tidskrift för genusvetenskap</title>
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+</head>
+
+<body>
+<p>Skriv nyhet</p>
+<form action="about_editors_process.php" method="post" enctype="multipart/form-data">
+    <ul>
+        <li>
+            <input type="text" name="fname" id="fname" placeholder="Förnamn">
+        </li>
+        <li>
+            <input type="text" name="lname" id="lname" placeholder="Efternamn">
+        </li>
+        <li>
+            <textarea name="content" id="content" placeholder="Info om redaktör"></textarea>
+        </li>
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Ladda upp" name="submit">
+    </ul>
+</form>
+</body>
+</html>
