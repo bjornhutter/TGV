973e8014965b0d9d9bc54a6904a7368103e66d81
Arvid Johansson <arvid.f.johansson@gmail.com>
2016-04-20 22:07:30 +0200

    created aside and layout of elements

--- index.php
+++ index.php
@@ -16,52 +16,6 @@
 
 </header>
 <?php include('includes/navigation.inc') ?>
-<section class="cfp">
-    <div>
-        <ul>
-            <li>
-                <p>Tema 1</p>
-            </li>
-            <li>
-                <p>Tema 2</p>
-            </li>
-            <li>
-                <p>Tema 3</p>
-            </li>
-        </ul>
-    </div>
-    <a href="send_script.php" class="send-script-button">Skicka in ditt manus!</a>
-</section>
-<section class="news-feed">
-    <h1 class="news-main-title">Nyheter</h1>
-
-    <?php
-
-    include ('includes/db_connect.inc');
-
-
-    $result = mysqli_query($link, "SELECT * FROM tgv_news ORDER BY date DESC") or die (mysqli_error($link));
-
-    echo '<div class="news-post-container>';
-    while ($row = mysqli_fetch_array($result)) {
-        $title = $row['title'];
-        $content = $row ['content'];
-        $date = $row ['date'];
-        $id = $row ['id'];
-
-        echo '<div class="news-post">';
-        echo '<p class="news-date">' . $date . '</p>';
-        echo '<h2 class="news-title">' . $title . '</h2>';
-        echo '<p class="news-content">' . nl2br($content) . '</p>';
-
-        echo '<a href="" >Redigera inlägg<!-- skickas till dashboard? --></a>';
-        echo '<hr>';
-        echo '</div>';
-    }
-    echo '</div>';
-
-    ?>
-</section>
 
 <main>
     <ul class="recent-article-wrapper">
@@ -88,7 +42,54 @@
         </li>
     </ul>
 </main>
+<aside>
+    <section class="cfp">
+        <div>
+            <ul>
+                <li>
+                    <p>Tema 1</p>
+                </li>
+                <li>
+                    <p>Tema 2</p>
+                </li>
+                <li>
+                    <p>Tema 3</p>
+                </li>
+            </ul>
+        </div>
+        <a href="send_script.php" class="send-script-button">Skicka in ditt manus!</a>
+    </section>
+    <section class="news-feed">
+        <h1 class="news-main-title">Nyheter</h1>
 
+        <?php
+
+        include ('includes/db_connect.inc');
+
+
+        $result = mysqli_query($link, "SELECT * FROM tgv_news ORDER BY date DESC") or die (mysqli_error($link));
+
+        echo '<div class="news-post-container>';
+        while ($row = mysqli_fetch_array($result)) {
+            $title = $row['title'];
+            $content = $row ['content'];
+            $date = $row ['date'];
+            $id = $row ['id'];
+
+            echo '<div class="news-post">';
+            echo '<p class="news-date">' . $date . '</p>';
+            echo '<h2 class="news-title">' . $title . '</h2>';
+            echo '<p class="news-content">' . nl2br($content) . '</p>';
+
+            echo '<a href="" >Redigera inlägg<!-- skickas till dashboard? --></a>';
+            echo '<hr>';
+            echo '</div>';
+        }
+        echo '</div>';
+
+        ?>
+    </section>
+</aside>
 
 <?php include('includes/footer.inc') ?>
 </body>
