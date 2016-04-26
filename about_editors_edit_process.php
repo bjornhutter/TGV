037c31bcc55de87d01ca03ba89e715b894b1bbd7
Julia Nilstal <nilstal@hotmail.com>
2016-04-26 17:55:19 +0200

    fixed errors

--- about_editors_edit_process.php
+++ about_editors_edit_process.php
@@ -12,8 +12,8 @@ if (isset($_POST['delete'])) {
     mysqli_query($link, "DELETE FROM tgv_about_editors WHERE id = '$id'");
 }
 elseif (isset($_POST['save'])) {
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
+    $fname= $_POST['fname'];
+    $lname= $_POST['lname'];
     $content = $_POST['content'];
     mysqli_query($link, "UPDATE tgv_about_editors SET fname = '$fname', lname = '$lname', content = '$content' WHERE id = '$id'");
 }
