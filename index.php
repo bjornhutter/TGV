08286eb6d21f3ad2613a2040bca99e99aae2e6b7
Julia Nilstal <nilstal@hotmail.com>
2016-04-26 20:12:29 +0200

    fixed active nav file, added in head

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
     <script src="scripts/stickynav.js"></script>
+    <script src="js/active_nav.js"></script>
+
 </head>
 <body>
 
