e14e3e59677bd7055092fbd71855384a3bd5ec17
Julia Nilstal <nilstal@hotmail.com>
2016-05-02 17:01:18 +0200

    added google map script

--- contact.php
+++ contact.php
@@ -9,8 +9,28 @@
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+    <script>
+        var myLatLng = new google.maps.LatLng(59.406517, 13.582419);
+        function initialize() {
+            var map = new google.maps.Map(document.getElementById("map"),
+                {
+                    zoom: 17,
+                    center: myLatLng,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                });
+
+            var marker = new google.maps.Marker(
+                {
+                    position: myLatLng,
+                    map: map,
+                    title: 'TGV'
+                });
+        }
+    </script>
+
 </head>
-<body>
+<body onload="initialize()" onunload="GUnload()">
 
 <header>
 
@@ -44,7 +64,7 @@ while ($row = mysqli_fetch_array($result)) {
 }
 ?>
 <section class="contact-map">
-    <div class="map">
+    <div id="map">
 
     </div>
 </section>
