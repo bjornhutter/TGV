c4075288089f170dd510207bc577ab003dfc2195
Julia Nilstal <nilstal@hotmail.com>
2016-05-02 15:32:28 +0200

    added script for changing filename

--- about_editors_process.php
+++ about_editors_process.php
@@ -2,6 +2,7 @@
 include('includes/db_connect.inc');
 //require('includes/auth.php');
 
+$temp_filename = $_FILES["fileToUpload"]["tmp_name"];
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
@@ -19,12 +20,6 @@ if (isset($_POST["submit"])) {
     }
 }
 
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Filen existerar redan.";
-    $uploadOk = 0;
-}
-
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "Filen är för stor";
@@ -37,29 +32,43 @@ if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpe
     $uploadOk = 0;
 }
 
+// Split the filename at every dot, take the last part and put it in $ext, (this will be the file extension)
+$splits = explode(".", $target_file);
+$index = count($splits) - 1;
+$ext  = '.'.$splits[$index];
+
+// Changes filename to an 8-digit number, if file exists it adds 1 to the number until the filename is unique
+$new_filename_basenumber = 10000000;
+$new_filename = $new_filename_basenumber.$ext;
+
+while (file_exists("uploads/$new_filename")) {
+    $new_filename_basenumber++;
+    $new_filename = $new_filename_basenumber.$ext;
+}
+
+// Move the file to the server and give it the generated name.
+move_uploaded_file($temp_filename, "uploads/$new_filename");
+
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     echo "Din fil blev ej uppladdad.";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
-        chmod($target_file, 0755);
+   // if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+   //     echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+       chmod("uploads/$new_filename", 0755);
 
-        $imgName = basename($_FILES["fileToUpload"]["name"]);
+        //$imgName = basename($_FILES["fileToUpload"]["name"]);
 
         $fname = $_POST['fname'];
         $lname = $_POST['lname'];
         $content = $_POST['content'];
 
-        mysqli_query($link, "INSERT INTO tgv_about_editors (fname, lname, content, image) VALUES ('$fname', '$lname', '$content', '$imgName')") or die(mysqli_error());
-        
-    } else {
-        echo "Det blev ett fel när du försökte ladda upp din fil.";
+        mysqli_query($link, "INSERT INTO tgv_about_editors (fname, lname, content, image) VALUES ('$fname', '$lname', '$content', '$new_filename')") or die(mysqli_error());
+
     }
-}
+// }
 
 header('Location: about.php')
 
 ?>
-
