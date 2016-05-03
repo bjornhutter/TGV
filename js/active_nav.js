8f1886b4969e7641e3fe1362fbb290187356ba27
Julia Nilstal <nilstal@hotmail.com>
2016-05-03 13:59:24 +0200

    new script for active links to fix index not getting active styling

--- js/active_nav.js
+++ js/active_nav.js
@@ -1,8 +1,13 @@
-$(function() {
-    var pgurl = window.location.href.substr(window.location.href
-            .lastIndexOf("/")+1);
-    $("nav ul li a").each(function(){
-        if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
-            $(this).addClass("active");
-    })
+jQuery(document).ready(function($){
+    // Get current path and find target link
+    var path = window.location.pathname.split("/").pop();
+
+    // Account for home page with empty path
+    if ( path == '' ) {
+        path = 'index.php';
+    }
+
+    var target = $('nav a[href="'+path+'"]');
+    // Add active class to target link
+    target.addClass('active');
 });
