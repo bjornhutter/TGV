e5ee48bc13113dbc636b80241656c588290b08c6
Julia Nilstal <nilstal@hotmail.com>
2016-05-03 14:00:05 +0200

    removed comments

--- js/active_nav.js
+++ js/active_nav.js
@@ -1,13 +1,8 @@
-jQuery(document).ready(function($){
-    // Get current path and find target link
+jQuery(document).ready(function ($) {
     var path = window.location.pathname.split("/").pop();
-
-    // Account for home page with empty path
-    if ( path == '' ) {
+    if (path == '') {
         path = 'index.php';
     }
-
-    var target = $('nav a[href="'+path+'"]');
-    // Add active class to target link
+    var target = $('nav a[href="' + path + '"]');
     target.addClass('active');
 });
