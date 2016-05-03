f4abca7d854a0424ba8cd630a4a7eaf0c62585ef
Julia Nilstal <nilstal@hotmail.com>
2016-05-03 13:38:53 +0200

    added WYSIWYG to form and editform for recent articles and about editors

--- recent_articles_edit.php
+++ recent_articles_edit.php
@@ -27,6 +27,8 @@ $imgName = $row['image'];
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+    <script>tinymce.init({ selector:'textarea' });</script>
 </head>
 
 <body>
