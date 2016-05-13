8e489246f14d4c6e03092c316837da9c74242a6d
Arvid Johansson <arvid.f.johansson@gmail.com>
2016-05-13 12:51:56 +0200

    changed to static subject bc didn't work with variable

--- send_script_process.php
+++ send_script_process.php
@@ -1,21 +1,26 @@
 <?php
-
 require_once 'libs/swiftmailer-5.x/lib/swift_required.php';
-$transport = Swift_SmtpTransport::newInstance()
-    ->setHost('smtp.tegeve.se')
-    ->setPort(587)
-    /*->setEncryption('ssl')*/;  //varför ssl inte funkar/ om det behövs
+if (isset($_POST['submit'])) {
+    $from = $_POST['from'];
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
 
-$mailer = Swift_Mailer::newInstance($transport);
+    $transport = Swift_SmtpTransport::newInstance()
+        ->setHost('smtp.tegeve.se')
+        ->setPort(587)/*->setEncryption('ssl')*/
+    ;  //varför ssl inte funkar/ om det behövs
 
-$message = Swift_Message::newInstance($_POST['message'])
-    ->setFrom(array($_POST['from']))
-    ->setTo(array('test@tegeve.se'))
-    ->setBody($_POST['message'])
-    ->attach(Swift_Attachment::fromPath('emailTest.txt'));
+    $mailer = Swift_Mailer::newInstance($transport);
 
-$result = $mailer->send($message);
-    
-    
+    $message = Swift_Message::newInstance()
+        ->setFrom(array($from => $fname . " " . $lname))
+        ->setTo(array('test@tegeve.se'))
+        ->setSubject('Manus från ' . $fname . " " . $lname)
+        ->setBody($_POST['message'])
+        ->attach(Swift_Attachment::fromPath('emailTest.txt'));
+
+    $result = $mailer->send($message);
+}
+
 
 //header('Location:send_script_complete.php');
