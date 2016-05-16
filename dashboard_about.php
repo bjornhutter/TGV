2c1ea4427dc95b78a1dcb5c9a02036026a633c4d
Bjorn Hutter <bjornhutter@gmail.com>
2016-05-16 11:26:11 +0200

    Changed tinyMCE

--- dashboard_about.php
+++ dashboard_about.php
@@ -13,7 +13,13 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
         <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
-        <script>tinymce.init({selector: 'textarea'});</script>
+        <script>
+            tinymce.init({
+                selector: 'textarea',
+                toolbar: 'undo redo | bold italic | bullist numlist',
+                menubar: 'file edit view'
+            });
+        </script>
         <script src="js/active_dashnav.js"></script>
     </head>
     <body>
