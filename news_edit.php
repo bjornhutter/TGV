ea77c8915450f914b84e2478cc9eb8805c3526fc
Arvid Johansson <arvid.f.johansson@gmail.com>
2016-05-20 12:27:17 +0200

    structure done, update/delete not working

--- news_edit.php
+++ news_edit.php
@@ -17,29 +17,72 @@ $newsContent = $row['content'];
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/css-reset.css">
+    <!--<link rel="stylesheet" type="text/css" href="css/master.css">-->
+    <link rel="stylesheet" type="text/css" href="css/dashboard.css">
+    <title>Dashboard | Tidskrift för genusvetenskap</title>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600,700,600italic' rel='stylesheet'
+          type='text/css'>
+    <link rel="stylesheet"
+          href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <!--<script src="js/stickynav.js"></script>-->
+    <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+    <script>
+        tinymce.init({
+            selector: 'textarea',
+            toolbar: 'undo redo | bold italic | bullist numlist | link code',
+            menubar: 'file edit view insert tools',
+            plugins: 'link code'
+        });
+    </script>
+    <script src="js/active_dashnav.js"></script>
 </head>
 <body>
-<main id="main">
+<header class="admin-header">
+    <h1 class="header-title">Admin Dashboard</h1>
+    <img src="img/icons/menu_white_revorked.svg" alt="Meny" class="toggle-nav" title="Meny">
+</header>
+<div id="site-wrapper">
+    <div id="site-canvas">
+        <div class="nav-main-wrapper">
+            <?php include('includes/dashboard_nav.inc') ?>
+            <div class="overview-wrapper">
+                <h1 class="dashboard-title">Hem</h1>
+                <a href="index.php" class="go-back-link" target="_blank" title="Öppnas på ny flik">Gå till Hem</a>
+            </div>
+            <div class="main-outer-wrapper">
+                <main id="main">
 
-    <form action="dashboard_process.php" method="post" class="dashboard-form">
-        <h2 class="dashboard-sub-title">Nyhetsflöde</h2>
-        <ul>
-            <li>
-                <p class="dashboard-first-form-title">Titel: </p>
-                <input type="text" name="newsTitle" title="Nyhetsflöde Titel" value="<?php echo $newsTitle ?>">
-            </li>
-            <li>
-                <p class="dashboard-form-title">Beskrivning: </p>
+                    <form action="news_edit_process.php" method="post" class="dashboard-form">
+                        <h2 class="dashboard-sub-title">Nyhetsflöde</h2>
+                        <ul>
+                            <li>
+                                <p class="dashboard-first-form-title">Titel: </p>
+                                <input type="text" name="newsTitle" title="Nyhetsflöde Titel"
+                                       value="<?php echo $newsTitle ?>">
+                            </li>
+                            <li>
+                                <p class="dashboard-form-title">Beskrivning: </p>
                 <textarea name="newsContent" title="Nyhetsflöde Beskrivning"
                           rows="10"><?php echo $newsContent ?></textarea>
-            </li>
-            <li>
-                <input type="submit" name="newsSubmit" value="Spara ändringar"
-                       class="form-input-submit">
-            </li>
-        </ul>
-    </form>
-</main>
+                            </li>
+                            <li>
+                                <input type="submit" name="newsSubmit" value="Spara ändringar"
+                                       class="form-input-submit">
+                            </li>
+                            <li>
+                                <input type="submit" name="newsDelete" value="Radera inlägg"
+                                       class="form-input-submit">
+                            </li>
+                        </ul>
+                    </form>
+                </main>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
