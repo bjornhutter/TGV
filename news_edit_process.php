dac1a915f0bef81b1badca9ca550660ca0ec9aa0
Arvid Johansson <arvid.f.johansson@gmail.com>
2016-05-20 13:48:54 +0200

    changed order

--- news_edit_process.php
+++ news_edit_process.php
@@ -1,16 +1,18 @@
 <?php
-require('includes/auth.inc');
+//require('includes/auth.inc');
 include('includes/db_connect.inc');
 
 $id = $_POST['id'];
 
-if (isset($_POST['newsSubmit'])) {
-    $newsTitle = $_POST['title'];
-    $newsContent = $_POST['content'];
+
+if (isset($_POST['newsDelete'])) {
+    mysqli_query($link, "DELETE FROM tgv_news WHERE id = '$id'");
+} elseif (isset($_POST['newsSubmit'])) {
+
+    $newsTitle = $_POST['newsTitle'];
+    $newsContent = $_POST['newsContent'];
 
     mysqli_query($link, "UPDATE tgv_news SET title = '$newsTitle', content = '$newsContent' WHERE id = '$id'");
-} elseif (isset($_POST['newsDelete'])) {
-    mysqli_query($link, "DELETE FROM tgv_news WHERE id = '$id'");
 }
 
-header("Location: news_posts.php"); 
+//header("Location: news_posts.php"); 
