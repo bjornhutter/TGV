81ce21853ddf59a0e7926f60feb4caa4c1df9825
Julia Nilstal <nilstal@hotmail.com>
2016-05-20 21:24:33 +0200

    changed link in cfp to dashboard

--- index.php
+++ index.php
@@ -49,7 +49,6 @@ if (!isset($_SESSION)) {
             echo '<img src="uploads/' . $imgName . '" class="recent-article-img">';
             echo '<h1 class="recent-article-title">' . $title . '</h1>';
             echo '<p class="recent-article-content">' . $content . '... <a class="recent-article-btn" href="articles_read_more.php?id='.$id.'">[Läs mer]</a></p>';
-            //FIXA RÄTT LÄNK FÖR REDIGERING
             if (isset($_SESSION['user'])) {
             echo '<p><a href="recent_articles_edit.php?id=' . $id . '" class="edit">Redigera</a></p>';
             }
@@ -74,7 +73,7 @@ if (!isset($_SESSION)) {
     echo '<p>' . $cfpContent . '</p>';
 
     if (isset($_SESSION['user'])) {
-        echo '<p><a href="dashboard_contact.php" class="edit">Redigera</a></p>';
+        echo '<p><a href="dashboard.php" class="edit">Redigera</a></p>';
     }
     echo '</section>';
 
