4beef76b4ec56a6e11e592f26d463fa6cf0cacaa
Julia Nilstal <nilstal@hotmail.com>
2016-05-20 13:36:24 +0200

    removed old edit form from recent articles edit

--- recent_articles_edit.php
+++ recent_articles_edit.php
@@ -41,31 +41,6 @@ $imgName = $row['image'];
 </head>
 <body>
 
-<p>Redigera senaste nummer</p>
-<form action="recent_articles_edit_process.php" method="post"  enctype="multipart/form-data">
-    <ul>
-        <li>
-            <input type="text" name="title" id="title" value="<?php echo $title ?>">
-        </li>
-        <li>
-            <textarea name="content" id="content"><?php echo $content ?></textarea>
-        </li>
-        <li>
-            <textarea name="featured" id="featured"><?php echo $featured ?></textarea>
-        </li>
-        <li>
-            <p>Ladda upp en ny bild (välj ingen fil, om du önskar ha kvar samma): </p>
-            <input type="file" name="newfileToUpload" id="newfileToUpload">
-        </li>
-        <li>
-            <input type="submit" name="save" value="Spara ändringar">
-        </li>
-        <li>
-            <input type="submit" name="delete" value="Radera inlägget" onClick="return confirm('Radera. Är du säker?')">
-        </li>
-    </ul>
-    <input type="hidden" value="<?php echo $id ?>" name="id">
-</form>
 
 <header class="admin-header">
     <h1 class="header-title">Admin Dashboard</h1>
