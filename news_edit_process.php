da3b2b73634de61f9e595db92c35d7669bc763e2
Arvid Johansson <arvid.f.johansson@gmail.com>
2016-05-20 15:56:13 +0200

    changed variables

--- news_edit_process.php
+++ news_edit_process.php
@@ -6,13 +6,18 @@ $id = $_POST['id'];
 
 
 if (isset($_POST['newsDelete'])) {
+    $id = $_POST['id'];
+
     mysqli_query($link, "DELETE FROM tgv_news WHERE id = '$id'");
 } elseif (isset($_POST['newsSubmit'])) {
+    $id = $_POST['id'];
 
-    $newsTitle = $_POST['newsTitle'];
-    $newsContent = $_POST['newsContent'];
+    $title = $_POST['newsTitle'];
+    $content = $_POST['newsContent'];
 
-    mysqli_query($link, "UPDATE tgv_news SET title = '$newsTitle', content = '$newsContent' WHERE id = '$id'");
+    mysqli_query($link, "UPDATE tgv_news SET title = '$title', content = '$content' WHERE id = '$id'");
 }
 
-//header("Location: news_posts.php"); 
+
+
+header("Location: news_posts.php"); 
