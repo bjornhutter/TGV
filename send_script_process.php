3657a6c8607a7afa84641ac1e9453f3329b359f7
Arvid Johansson <arvid.f.johansson@gmail.com>
2016-05-29 15:31:48 +0200

    done

--- send_script_process.php
+++ send_script_process.php
@@ -15,70 +15,164 @@ if (isset($_POST['submit'])) {
 
     $attachment = Swift_Attachment::fromPath($_FILES['attachFile']['tmp_name'])->setFilename($_FILES['attachFile']['name']);
 
-    //$img = Swift_Image::fromPath('http://www.tegeve.se/public_html/newsite/tgv_arvid/img/tgv_email_header.png');
-
     $message = Swift_Message::newInstance()
         ->setFrom(array($from => $fname . " " . $lname))
-        ->setTo(array('noreply@tegeve.se', 'arvid.f.johansson@gmail.com'))//ändra till tegeve@oru.se
+        ->setTo(array('noreply@tegeve.se', /* ersätt med 'tegeve@oru.se' när arbetet är helt klart*/))
         ->setSubject('Manus från ' . $fname . " " . $lname)
         ->setMaxLineLength(78)
         ->attach($attachment)
-        ->setBody($emailMessage, 'text/plain')
-        ->addPart(
+        ->setBody(
             '<html>' .
             '<head></head>' .
             '<body>' .
-            '<table align="center" style="width:600px;height:700px;">' .
-            // header img
+            '<table align="center" style="width:600px;font-family:‘Trebuchet MS’, sans-serif">' .
             '<tr style="color:#fff;background-color:#e07929;">' .
-            '<td align="center" style="height:60px;font-size:30px">TIDSKRIFT FÖR GENUSVETENSKAP</td>' .
-            //'<td><img src="' . $img . '" alt="image"/></td>' .
+            '<td colspan="2" align="center" style="height:60px;font-family:Franklin Gothic Medium;font-size:30px">TIDSKRIFT FÖR GENUSVETENSKAP</td>' .
+            '</tr>' .
+            '<tr style="height:50px;"></tr>' . //whitespace
+
+            // title 
+            '<tr>' .
+            '<td style="width:15px;"></td>' .
+            '<td style="font-size: 20px;">' . $fname . " " . $lname . ' har skickat in manus till TGV</td>' .
+            '</tr>' .
+            '<tr style="height:50px;">' .  //whitespace
+            '<td></td>' .
+            '</tr>' .
+
+            // message
+            '<tr>' .
+            '<td></td>' .
+            '<td>' . $emailMessage . '</td> ' .
+            '</tr>' .
+            '</table>' .
+            '<p style="height:200px"></p>' . //whitespace
+
+            // footer
+            '<table align="center" style="font-family:Verdana, Geneva, sans-serif;width:600px;color:white;background-color:#252525;">' .
+            '<tr style="height:20px;">' .
+            '<td style="width: 40px;"></td>' .
+            '<td></td>' .
+            '</tr>' .
+            '<tr style="font-size:20px;">' .
+            '<td></td>' .
+            '<td>Kontakt</td>' .
+            '</tr>' .
+            '<tr>' .
+            '<td></td>' .
+            '<td style="height:20px;"></td>' .
+            '</tr>' .
+            '<tr>' .
+            '<td></td>' .
+            '<td>054-700 1000</td>' .
+            '</tr>' .
+            '<tr>' .
+            '<td></td>' .
+            '<td>tegeve@oru.se</td>' .
+            '</tr>' .
+            '<tr>' .
+            '<td></td>' .
+            '<td>Universitetsgatan 2</td>' .
+            '</tr>' .
+            '<tr>' .
+            '<td></td>' .
+            '<td>651 88 Karlstad</td>' .
+            '</tr>' .
+            '<tr style="height:20px;">' .
+            '<td></td>' .
+            '</tr>' .
+            '<tr>' .
+            '<td></td>' .
+            '<td><a href="http://tegeve.se/">Till tegeve.se</a></td>' .
+            '</tr>' .
+            '<tr style="height: 20px;">' .
+            '<td></td>' .
+            '</tr>' .
+            '</table>' .
+            '</body>' .
+            '</html>',
+            'text/html')
+        ->addPart($emailMessage, 'text/plain');
+
+    $message2 = Swift_Message::newInstance()
+        ->setFrom(array('noreply@tegeve.se' => "Tidskrift för genusvetenskap"))
+        ->setTo(array($from))
+        ->setSubject('Bekräftelse - manus till Tidskrift för genusvetenskap')
+        ->setMaxLineLength(78)
+        ->setBody(
+            '<html>' .
+            '<head>' .
+            '</head>' .
+            '<body>' .
+            '<table border="0" align="center" style="width:600px;font-family:‘Trebuchet MS’, sans-serif">' .
+            '<tr style="color:#fff;background-color:#e07929;">' .
+            '<td colspan="2" align="center" style="height:60px;font-family:Franklin Gothic Medium;font-size:30px">TIDSKRIFT FÖR GENUSVETENSKAP' .
+            '</td>' .
             '</tr>' .
             '<tr style="height:50px;"></tr>' . //whitespace
 
             // title
             '<tr>' .
-            '<td><h3 style="">' . $fname . " " . $lname . ' har skickat in manus till TGV</h3></td>' .
+            '<td style="width:15px;"></td>' .
+            '<td style="font-size: 20px;">Tack för att du skickade in manus till TGV</td>' .
             '</tr>' .
-            '<tr style="height:50px;"><td></td></tr>' . //whitespace
-
-            // message
-            '<tr>' .
-            '<td>' . $emailMessage . '</td>' .
+            '<tr style="height:50px;">' .  //whitespace
+            '<td></td>' .
             '</tr>' .
             '</table>' .
-            '<tr style="height:50px;"></tr>' . //whitespace
+            '<p style="height:200px"></p>' . //whitespace
 
             // footer
-            '<table align="center" style="width:600px;color:white;background-color:#252525;">' .
+            '<table border="0" align="center" style="font-family:Verdana, Geneva, sans-serif;width:600px;color:white;background-color:#252525;">' .
             '<tr style="height:20px;">' .
-            //'<td></td>'.
+            '<td style="width: 40px;"></td>' .
+            '<td></td>' .
             '</tr>' .
-            '<tr>' .
+            '<tr style="font-size:20px;">' .
+            '<td></td>' .
             '<td>Kontakt</td>' .
             '</tr>' .
             '<tr>' .
+            '<td></td>' .
+            '<td style="height:20px;"></td>' .
+            '</tr>' .
+            '<tr>' .
+            '<td></td>' .
             '<td>054-700 1000</td>' .
             '</tr>' .
             '<tr>' .
+            '<td></td>' .
             '<td>tegeve@oru.se</td>' .
             '</tr>' .
             '<tr>' .
+            '<td></td>' .
             '<td>Universitetsgatan 2</td>' .
             '</tr>' .
             '<tr>' .
+            '<td></td>' .
             '<td>651 88 Karlstad</td>' .
             '</tr>' .
+            '<tr style="height:20px;">' .
+            '<td></td>' .
+            '</tr>' .
             '<tr>' .
-            '<td><a href="http://tegeve.se/">Till TGV</a></td>' .
+            '<td></td>' .
+            '<td><a href="http://tegeve.se/">Till tegeve.se</a></td>' .
+            '</tr>' .
+            '<tr style="height: 20px;">' .
+            '<td></td>' .
             '</tr>' .
             '</table>' .
             '</body>' .
             '</html>',
-            'text/html');
+            'text/html')
+        ->addPart('Tack för att du skickade in manus till TGV', 'text / plain'
+        );
 
     $result = $mailer->send($message);
+    $result = $mailer->send($message2);
 }
 
 
-header('Location:send_script_complete.php');
+
+//header('Location:send_script_complete . php');
