dc96c5e684de60672eac627f51016bf0a1c5cb8f
Arvid Johansson <arvid.f.johansson@gmail.com>
2016-05-29 15:49:13 +0200

    added correct head tags

--- send_script_complete.php
+++ send_script_complete.php
@@ -3,14 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description"
+          content="Anvisningar för artikelskribenter, recensenter och granskare. Skicka in ditt manus till TGV via formuläret!">
     <link rel="stylesheet" type="text/css" href="css/css-reset.css">
     <link rel="stylesheet" type="text/css" href="css/master.css">
-    <title>Tidskrift för genusvetenskap</title>
+    <title>Skicka manus | Tidskrift för genusvetenskap</title>
     <link rel="icon" href="img/tgv_favicon.ico">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600,700,600italic' rel='stylesheet'
+          type='text/css'>
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <script src="js/active_nav.js"></script>
     <script src="js/stickynav.js"></script>
+    <script src="js/nav_mobile_toggle.js"></script>
 </head>
 <body>
 <?php include ('includes/navigation.inc')?>
@@ -18,7 +24,6 @@
     <section class="email-sent-container">
         <ul>
             <li><h1>Tack! Manus skickat.</h1></li>
-            <li><p>Vi kontaktar dig inom kort!</p></li>
         </ul>
     </section>
 </main>
